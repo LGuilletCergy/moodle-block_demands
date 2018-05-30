f01cf0424efdd1c72e412f6e3be6b0461c8cef11
LGuilletCergy <superviseur@por-hi-sef-lgui.administration.adm>
2018-05-30 10:41:52 +0200

    Corrected plugin component name

--- version.php
+++ version.php
@@ -35,7 +35,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018053000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;        // Requires this Moodle version
-$plugin->component = 'block_enrol_demands'; // Full name of the plugin (used for diagnostics)
+$plugin->component = 'block_demands'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array(
     'enrol_demands' => 2018053000,   // The enrolment method demands must be present
     //(version 2018053000).
